336733c5ea7ad3091904325bf221c8eb4fd7bd14
jorabalo97 <jorabalo97@gmail.com>
2023-11-20 10:45:19 +0100

    Initial Commit

---
+++ Calculadora2.0/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Calculadora2.0
+//
+//  Created by Jorge Abalo Dieste on 20/11/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    // - Private methods
+    
+    private func setupView(){
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Calculadora2.0/Controller/Home/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Calculadora2.0
+//
+//  Created by Jorge Abalo Dieste on 20/11/23.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // Life Cycle
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+ 
+}
